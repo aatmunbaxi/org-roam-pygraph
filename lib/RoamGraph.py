6b8aa7403f50c296de465cff5f281bc29a48ed33
atmnbaxi <abaxi@tamu.edu>
2023-12-07 13:46:14 -0600

    Final fix of =__init_tags()= logic

--- lib/RoamGraph.py
+++ lib/RoamGraph.py
@@ -191,7 +191,7 @@ class RoamGraph:
                 csr = con.cursor()
                 query = csr.execute(tags_query)
                 clean = lambda s: s.replace('"', "")
-                match_null = lambda s : set() if not s else s[1].split(",")
+                match_null = lambda s : set() if not s else s.split(",")
                 return [set(map(clean, match_null(i[1]))) for i in query.fetchall()]
 
         except sql.Error as e:
